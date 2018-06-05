6e050c4791076c97f0926db821ae6331efba432a
james2008//git <1179019815@qq.com>
2018-06-05 18:55:31 +0800

    侧拉1.0

--- app/src/main/java/com/bawei/myyikezhong/MainActivity.java
+++ app/src/main/java/com/bawei/myyikezhong/MainActivity.java
@@ -2,10 +2,16 @@ package com.bawei.myyikezhong;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.LinearLayout;
 
 import com.bawei.myyikezhong.Duanzi.DuanziFragment;
 import com.bawei.myyikezhong.Qutu.QutuFragment;
@@ -13,6 +19,11 @@ import com.bawei.myyikezhong.Shipin.ShipinFragment;
 import com.bawei.myyikezhong.Tuijian.TuijianFragment;
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.hjm.bottomtabbar.BottomTabBar;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private BottomTabBar mBtb;
@@ -22,13 +33,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     private TextView mTv;
     private ImageView mImgbtn;
-
+    private ListView mLv;
+    List<Map<String, Object>> list;
+    private LinearLayout mLl;
+    private DrawerLayout mDl;
+    //侧拉框的数据
+    int imgarr[] = {R.mipmap.raw_1499933216, R.mipmap.raw_1499947358, R.mipmap.raw_1499946865, R.mipmap.raw_1499947389};
+    final String tvarr[] = {"我的关注", "我的收藏", "搜索好友", "消息通知"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
-
 
 
         mBtb.init(getSupportFragmentManager())
@@ -41,6 +57,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 .addTabItem("视频", R.mipmap.raw_1500086067, R.mipmap.raw_1500083686, ShipinFragment.class)
                 .addTabItem("趣图", R.mipmap.raw_1500085367, R.mipmap.raw_1500083878, QutuFragment.class)
                 .isShowDivider(true);
+        // SimpleAdapter
+        SimpleAdapter simpleAdapter = new SimpleAdapter(this, getData(), R.layout.listview_item, new String[]{"ivPic", "tvText"}, new int[]{R.id.ivPic, R.id.tvtext});
+        mLv.setAdapter(simpleAdapter);
+        //mLv的点击事件
+        mLv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, tvarr[i], Toast.LENGTH_SHORT).show();
+                mDl.closeDrawer(mLl);
+            }
+        });
     }
 
     private void initView() {
@@ -50,10 +77,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mTv = (TextView) findViewById(R.id.tv);
         mImgbtn = (ImageView) findViewById(R.id.imgbtn);
         mImgbtn.setOnClickListener(this);
+        mLv = (ListView) findViewById(R.id.lv);
+
+        mLl = (LinearLayout) findViewById(R.id.ll);
+        mDl = (DrawerLayout) findViewById(R.id.activity_main);
     }
 
     /**
      * 图片点击
+     *
      * @param v
      */
     @Override
@@ -65,6 +97,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.sdv:
                 break;
+
         }
+    }
+
+    /**
+     * 侧拉的数据
+     *
+     * @return
+     */
+
+    private List<Map<String, Object>> getData() {
+
+        list = new ArrayList<Map<String, Object>>();
+        for (int i = 0; i < tvarr.length; i++) {
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("ivPic", imgarr[i]);    //显示的图片信息
+            map.put("tvText", tvarr[i]);                  //显示的文字信息
+            list.add(map);
+        }
+        return list;
     }
 }
