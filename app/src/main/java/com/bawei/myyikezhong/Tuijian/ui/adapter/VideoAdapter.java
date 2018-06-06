4a90a4ebae645d8ea985dc422e13abb5a6999325
1275021828@qq.com <1275021828@qq.com>
2018-06-06 21:17:58 +0800

    关注

--- app/src/main/java/com/bawei/myyikezhong/Tuijian/ui/adapter/VideoAdapter.java
+++ app/src/main/java/com/bawei/myyikezhong/Tuijian/ui/adapter/VideoAdapter.java
@@ -7,13 +7,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.VideoView;
 
 import com.bawei.myyikezhong.R;
 import com.bawei.myyikezhong.Tuijian.ui.bean.VideoBean;
 import com.facebook.drawee.view.SimpleDraweeView;
 
 import java.util.List;
+
+import fm.jiecao.jcvideoplayer_lib.JCVideoPlayerSimple;
+import fm.jiecao.jcvideoplayer_lib.JCVideoPlayerStandard;
 
 public class VideoAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
 
@@ -42,8 +44,12 @@ public class VideoAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
         videoViewHolder.imgtou.setImageURI(dataBean.getProfile_image());
         videoViewHolder.text1.setText(dataBean.getText());
         videoViewHolder.text2.setText(dataBean.getName());
-        videoViewHolder.videoView.setVideoPath(dataBean.getVideouri());
-        videoViewHolder.videoView.start();
+//        videoViewHolder.videoView.setVideoPath(dataBean.getVideouri());
+//        videoViewHolder.videoView.start();
+        String videouri = dataBean.getVideouri();
+
+        videoViewHolder.mVideoplayer.setUp(videouri, JCVideoPlayerStandard.SCREEN_LAYOUT_NORMAL, "视频播放");
+
 
     }
 
@@ -58,14 +64,16 @@ public class VideoAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
         private final SimpleDraweeView imgtou;
         private final TextView text2;
         private final TextView text1;
-        private final VideoView videoView;
+        public JCVideoPlayerSimple mVideoplayer;
+       // private final VideoView videoView;
 
         public VideoViewHolder(View itemView) {
             super(itemView);
             imgtou = itemView.findViewById(R.id.imgtou);
             text2 = itemView.findViewById(R.id.text2);
             text1 = itemView.findViewById(R.id.text1);
-            videoView = itemView.findViewById(R.id.surface_view);
+            //videoView = itemView.findViewById(R.id.surface_view);
+            mVideoplayer = itemView.findViewById(R.id.videoplayer);
         }
     }
 
