d8a37b73ac9cb3ba3f8246ae6bc178e69dfe69b3
james2008//git <1179019815@qq.com>
2018-06-10 21:43:58 +0800

    三方登录（QQ和微信）

--- app/src/main/java/com/bawei/myyikezhong/SanRegLogin/san/SanfangActivity.java
+++ app/src/main/java/com/bawei/myyikezhong/SanRegLogin/san/SanfangActivity.java
@@ -3,13 +3,24 @@ package com.bawei.myyikezhong.SanRegLogin.san;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bawei.myyikezhong.R;
 import com.bawei.myyikezhong.SanRegLogin.denluzhuce.LoginregActivity;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import cn.sharesdk.framework.Platform;
+import cn.sharesdk.framework.PlatformActionListener;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.tencent.qq.QQ;
+import cn.sharesdk.wechat.friends.Wechat;
 
 
 public class SanfangActivity extends AppCompatActivity implements View.OnClickListener {
@@ -46,10 +57,11 @@ public class SanfangActivity extends AppCompatActivity implements View.OnClickLi
                 break;
             case R.id.qq:
                 //使用QQ登录
-
+                    getQQ();
                 break;
             case R.id.weixin:
                 //使用微信登录
+                getWeiXin();
                 break;
             case R.id.other:
                 //其他方式登录  跳转到其他页面
@@ -58,7 +70,90 @@ public class SanfangActivity extends AppCompatActivity implements View.OnClickLi
                 break;
         }
     }
+    /*
+    * 第三方登录QQ
+    * */
+    public void getQQ(){
 
+        Platform plat = ShareSDK.getPlatform(QQ.NAME);
+        plat.removeAccount(true); //移除授权状态和本地缓存，下次授权会重新授权
+        plat.SSOSetting(false); //SSO授权，传false默认是客户端授权，没有客户端授权或者不支持客户端授权会跳web授权
+        plat.setPlatformActionListener(new PlatformActionListener() {//授权回调监听，监听oncomplete，onerror，oncancel三种状态
+            @Override
+            public void onComplete(Platform platform, int i, HashMap<String, Object> hashMap) {
+                Iterator iterator = hashMap.entrySet().iterator();
+                while (iterator.hasNext()){
+                    Map.Entry next = (Map.Entry) iterator.next();
+                    Object key = next.getKey();
+                    Object value = next.getValue();
+                    Log.d("哈哈", "onComplete: "+key+"  "+value);
+
+                }
+            }
+
+            @Override
+            public void onError(Platform platform, int i, Throwable throwable) {
+                Log.d("哈哈", "onError: "+platform.getName()+" "+platform.getDb()+"  "+"  "+platform.getId()+"  "+platform.getPlatformActionListener()+" "+platform.getVersion());
+            }
+
+            @Override
+            public void onCancel(Platform platform, int i) {
+                Log.d("哈哈", "onCancel: "+platform.getName());
+            }
+        });
+        if (plat.isClientValid()) {
+            //todo 判断是否存在授权凭条的客户端，true是有客户端，false是无
+        }
+        if (plat.isAuthValid()) {
+            //todo 判断是否已经存在授权状态，可以根据自己的登录逻辑设置
+            Toast.makeText(this, "已经授权过了", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        //plat.authorize();    //要功能，不要数据
+        plat.showUser(null);    //要数据不要功能，主要体现在不会重复出现授权界面
+    }
+    /*
+    * 第三方微信登录
+    * */
+    public void getWeiXin(){
+
+        Platform plat = ShareSDK.getPlatform(Wechat.NAME);
+        plat.removeAccount(true); //移除授权状态和本地缓存，下次授权会重新授权
+        plat.SSOSetting(false); //SSO授权，传false默认是客户端授权，没有客户端授权或者不支持客户端授权会跳web授权
+        plat.setPlatformActionListener(new PlatformActionListener() {//授权回调监听，监听oncomplete，onerror，oncancel三种状态
+            @Override
+            public void onComplete(Platform platform, int i, HashMap<String, Object> hashMap) {
+                Iterator iterator = hashMap.entrySet().iterator();
+                while (iterator.hasNext()){
+                    Map.Entry next = (Map.Entry) iterator.next();
+                    Object key = next.getKey();
+                    Object value = next.getValue();
+                    Log.d("哈哈", "onComplete: "+key+"  "+value);
+
+                }
+            }
+
+            @Override
+            public void onError(Platform platform, int i, Throwable throwable) {
+                Log.d("哈哈", "onError: "+platform.getName()+" "+platform.getDb()+"  "+"  "+platform.getId()+"  "+platform.getPlatformActionListener()+" "+platform.getVersion());
+            }
+
+            @Override
+            public void onCancel(Platform platform, int i) {
+                Log.d("哈哈", "onCancel: "+platform.getName());
+            }
+        });
+        if (plat.isClientValid()) {
+            //todo 判断是否存在授权凭条的客户端，true是有客户端，false是无
+        }
+        if (plat.isAuthValid()) {
+            //todo 判断是否已经存在授权状态，可以根据自己的登录逻辑设置
+            Toast.makeText(this, "已经授权过了", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        //plat.authorize();    //要功能，不要数据
+        plat.showUser(null);    //要数据不要功能，主要体现在不会重复出现授权界面
+    }
 
 
 }
